ecb42484100b3937e79a20b647bc42872182a781
Santiago Font <santifonlop@hotmail.com>
2024-10-14 23:44:12 -0300

    finished our work, finished what we offer

--- landing_page/js/app.js
+++ landing_page/js/app.js
@@ -13,3 +13,21 @@ var swiper = new Swiper(".swiper_trusted", {
     loop: true,
     speed: 3000
   });
+
+  var swiper = new Swiper(".swiper_ourwork", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
+
