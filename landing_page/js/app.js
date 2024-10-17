4637ad0cfd1f78b936880d275e77deb7b4f8ecc6
Santiago Font <santifonlop@hotmail.com>
2024-10-16 21:37:50 -0300

    finished step by step plan of action

--- landing_page/js/app.js
+++ landing_page/js/app.js
@@ -31,3 +31,97 @@ var swiper = new Swiper(".swiper_trusted", {
     },
   });
 
+let bar = document.getElementById("bar")
+let filled = document.getElementById("filled")
+
+
+
+
+function getCenterBarIndex() {
+  const bars = document.querySelectorAll('.step_numberInfo');
+  const viewportHeight = window.innerHeight;
+
+  let centerBarIndex = -1;
+  let closestDistance = viewportHeight;
+
+  bars.forEach((bar, index) => {
+    const rect = bar.getBoundingClientRect();
+    
+    const barCenter = rect.top + rect.height / 2;
+    const distanceFromCenter = Math.abs(barCenter - viewportHeight / 2);
+    
+    if (distanceFromCenter < closestDistance) {
+      closestDistance = distanceFromCenter;
+      centerBarIndex = index;
+    }
+  });
+  return centerBarIndex; 
+}
+
+const logicColor = (centerBarIndex) =>{
+  if(centerBarIndex == 0){
+    filled.style.height = "0%"
+    let a = document.querySelectorAll('.step_numberInfo .number')
+    for (let index = 0; index < a.length; index++) {
+      if(index <= centerBarIndex){
+        a[index].classList.add("number_filled")
+      }else{
+        a[index].classList.remove("number_filled")
+      }
+      
+    }
+  }
+  if(centerBarIndex == 1){
+    filled.style.height = "35%"
+    let a = document.querySelectorAll('.step_numberInfo .number')
+    for (let index = 0; index < a.length; index++) {
+      if(index <= centerBarIndex){
+        a[index].classList.add("number_filled")
+      }else{
+        a[index].classList.remove("number_filled")
+      }
+      
+    }
+  }
+  if(centerBarIndex == 2){
+    filled.style.height = "65%"
+    let a = document.querySelectorAll('.step_numberInfo .number')
+    for (let index = 0; index < a.length; index++) {
+      if(index <= centerBarIndex){
+        a[index].classList.add("number_filled")
+      }else{
+        a[index].classList.remove("number_filled")
+      }
+      
+    }
+  }
+  if(centerBarIndex == 3){
+    filled.style.height = "100%"
+    let a = document.querySelectorAll('.step_numberInfo .number')
+    for (let index = 0; index < a.length; index++) {
+      if(index <= centerBarIndex){
+        a[index].classList.add("number_filled")
+      }else{
+        a[index].classList.remove("number_filled")
+      }
+      
+    }
+  }
+}
+
+window.addEventListener('scroll', () => {
+  const centerBarIndex = getCenterBarIndex();
+  logicColor(centerBarIndex)
+  
+});
+
+window.addEventListener('resize', () => {
+  const centerBarIndex = getCenterBarIndex();
+  logicColor(centerBarIndex)
+});
+
+const initialCenterBarIndex = getCenterBarIndex();
+if (initialCenterBarIndex !== -1) {
+    logicColor(initialCenterBarIndex)
+}
+
